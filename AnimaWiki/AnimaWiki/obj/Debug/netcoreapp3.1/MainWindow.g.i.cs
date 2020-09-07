606a603afff3829a29c43c97277af3abd318d48f
Artem <artemermoalev0@gmail.com>
2020-09-07 12:51:37 +0300

    Revert "Add all stucture"
    This reverts commit 031f6b58df4d9678bdc5781a4b2ff9457df1f136.

--- AnimaWiki/AnimaWiki/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ AnimaWiki/AnimaWiki/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace AnimaWiki {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AnimaWiki;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AnimaWiki;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
